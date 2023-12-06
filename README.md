df6d11191ea9ea01f1a3292f29ad96dd24f38d2f
sbw <stephenbrodiewild@gmail.com>
2023-12-06 19:01:12 +0800

    Tidy up

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Omnidots API Client
 The Omnidots API docs are [here](https://honeycomb.omnidots.com/api/docs).
 
+Fed those docs to ChatGPT, told it to make an OpenAPI spec, then used a code [generator](https://github.com/deepmap/oapi-codegen) to get the Go client code
+
+## Run the example 
 ```bash
-go build -o example ./cmd/example
+# set your token 
+export OMNIDOTS_TOKEN=...
+go run cmd/example/main.go
 ```
--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/stephenbrodiewild/omnidots-api-client/pkg/client"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	ctx := context.Background()
+	server := "http://honeycomb.omnidots.com/api/v1"
+	token := os.Getenv("OMNIDOTS_TOKEN")
+
+	client, err := client.NewClientWithResponses(server, token)
+	if err != nil {
+		panic(fmt.Errorf("failed to initialise client: %s", err))
+
+	}
+
+	sensorsResponse, err := client.ListSensorsWithResponse(ctx)
+	if err != nil {
+		panic(fmt.Errorf("failed to list sensors: %s", err))
+	}
+
+	if sensorsResponse.JSON200 != nil {
+		for _, sensor := range *sensorsResponse.JSON200.Sensors {
+			fmt.Println(*sensor.Name)
+		}
+	}
 }
